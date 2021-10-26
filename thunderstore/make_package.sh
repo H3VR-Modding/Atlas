e9788273c7cee703b334a61fdcfe78a6e36341cf
Nathan Gill <nrgill28@gmail.com>
2021-10-25 20:50:37 -0400

    Added icon source

--- thunderstore/make_package.sh
+++ thunderstore/make_package.sh
@@ -4,25 +4,23 @@ export VERSION=$(git describe --tags --abbrev=0 | sed -n 's/v\(.\+\)/\1/p')
 cd $TS_DIR
 
 # Delete the existing build if it exists
-rm nrgill28-Sodalite-*.zip
+rm nrgill28-Atlas-*.zip
 
 # Create our temp folders
-mkdir -p TEMP/Sodalite/plugins
-mkdir TEMP/Sodalite/patchers
+mkdir -p TEMP/
 
 # Copy the files into them
 cp manifest.json TEMP/manifest.json
 cp icon.png TEMP/icon.png
 cp ../README.md TEMP/README.md
-cp ../src/Sodalite/bin/Release/net35/Sodalite.dll TEMP/Sodalite/plugins/Sodalite.dll
-cp ../src/Sodalite/bin/Release/net35/Sodalite.Patcher.dll TEMP/Sodalite/patchers/Sodalite.Patcher.dll
+cp ../src/Atlas/bin/Runtime/net35/Atlas.dll TEMP/Atlas.dll
 
 # Modify the version number
 sed -i "s/{VERSION}/$VERSION/g" TEMP/manifest.json
 
 # Zip the folder
 cd TEMP
-zip -9r "../nrgill28-Sodalite-$VERSION.zip" *
+zip -9r "../nrgill28-Atlas-$VERSION.zip" *
 
 # Delete the temp dir
 cd ..
